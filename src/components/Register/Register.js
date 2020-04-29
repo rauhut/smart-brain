7ca119792adb5a198f62945b69513a8cc99481c4
rauhut <jackierauhut@gmail.com>
2020-04-28 23:07:37 -0400

    fix styling issue that occured when using autocomplete on input fields

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -57,7 +57,7 @@ class Register extends React.Component {
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
                             <input 
-                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                                 type="text" 
                                 name="name"  
                                 id="name"
@@ -66,7 +66,7 @@ class Register extends React.Component {
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                             <input 
-                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                                 type="email" 
                                 name="email-address"  
                                 id="email-address"
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -52,7 +52,7 @@ class SignIn extends React.Component {
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                             <input 
-                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                                 type="email" 
                                 name="email-address"  
                                 id="email-address" 
